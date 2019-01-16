84e81d7243cd7b6e14d8cb6831b5e354299ed990
Piotr Chudziak <strangelove44@gmail.com>
2019-01-16 12:34:17 +0000

    add some dynamic routing

--- run.py
+++ run.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/dashboard/<user>')# Some dynamic routing to improve UX
+def dashboard(user):
+    return render_template('dashboard.html', user=user)
+
 if __name__ == '__main__':
 	app.run(debug = True)
