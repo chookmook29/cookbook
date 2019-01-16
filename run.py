c684ce6140bb8904423ee9ef060231d6213ce62f
Piotr Chudziak <strangelove44@gmail.com>
2019-01-16 13:26:49 +0000

    add more routing, remove dashboard.html, move links

--- run.py
+++ run.py
@@ -10,5 +10,25 @@ def index():
 def dashboard(user):
     return render_template('dashboard.html', user=user)
 
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+@app.route('/sign_out')
+def sign_out():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/show_recipes')
+def show_recipes():
+    return render_template('show_recipes.html')
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html')
+
 if __name__ == '__main__':
 	app.run(debug = True)
