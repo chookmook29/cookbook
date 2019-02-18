4add1144187d854ebcd10ff82d3928af6cadb43e
Piotr Chudziak <strangelove44@gmail.com>
2019-02-18 22:29:56 +0000

    add new test

--- test.py
+++ test.py
@@ -5,23 +5,51 @@ import unittest
 
 class FlaskTestCase(unittest.TestCase):
 
-	def setUp(self):#App initialize
-		self.app = app.test_client()
-		self.app.application.config['SECRET_KEY'] = ']Nk(`K24HLRuRkdN'
-		self.app.application.config['SESSION_COOKIE_DOMAIN'] = None
-		self.app.application.config["SERVER_NAME"] = "{0} {1}".format(os.environ.get('PORT'), os.environ.get('IP'))
-	
-	def test_initial(self):#Check if index.html has been created
-		with app.app_context():
-			response = self.app.get('/')
-			self.assertEqual(response.status_code, 200)
+    def setUp(self):#App initialize
+        self.app = app.test_client()
+        self.app.application.config['SECRET_KEY'] = ']Nk(`K24HLRuRkdN'
+        self.app.application.config['SESSION_COOKIE_DOMAIN'] = None
+        self.app.application.config["SERVER_NAME"] = "{0} {1}".format(os.environ.get('PORT'), os.environ.get('IP'))
+    
+    def test_initial(self):#Check if index.html has been created
+        with app.app_context():
+            response = self.app.get('/')
+            self.assertEqual(response.status_code, 200)
 
-	def test_show_all(self):#Check if show_all.html has been created
-		with app.app_context():
-			response = self.app.get('/show_all')
-			self.assertEqual(response.status_code, 200)
+    def test_show_all(self):#Check if show_all.html has been created
+        with app.app_context():
+            response = self.app.get('/show_all')
+            self.assertEqual(response.status_code, 200)
+            
+    def test_vote_logic(self):
+        voted_down = "bob"
+        voted_up = "frank"
+        downvotes =  3
+        upvotes =  4
+        user = "frank"
+        voted_down = str(voted_down)
+        voted_up = str(voted_up)
+        if user in voted_down:  
+            self.assertNotEqual(voted_up, "")
+        elif user in voted_up:
+            downvotes = int(downvotes)
+            downvotes = downvotes + 1
+            downvotes = str(downvotes)
+            voted_down = voted_down + user
+            upvotes = int(upvotes)
+            upvotes = upvotes - 1
+            upvotes = str(upvotes)
+            voted_up = voted_up.replace('frank', '')
+            self.assertEqual(voted_up, "")
+
+        else:
+            downvotes = int(downvotes)
+            downvotes = downvotes + 1
+            downvotes = str(downvotes)
+            voted_down = voted_down + user
+            self.assertNotEqual(voted_up, "")
 
 
 if __name__ == '__main__':
-	
-	unittest.main()
+    
+    unittest.main()
